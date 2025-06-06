33bcf6dcd6f1200c977c1782e11c9740c9c0b925
roberth <roberthgraffegraffe@gmail.com>
2025-06-06 12:27:02 -0400

    mover archivos a directorio de prueba y agregar codigo para brujula

---
+++ test/brujula.py
@@ -0,0 +1,19 @@
+import raspy_qmc5883l
+import time
+
+sensor = raspy_qmc5883l.QMC5883L(i2c_bus=4)
+m = sensor.get_magnet()
+print(m)
+
+# Calibrar
+sensor.calibration = [
+    [1.3166078114576967, -0.06412371173670786, -1874.4407833172772],
+    [-0.06412371173670785, 1.0129872045416726, -3436.0563916932297],
+    [0.0, 0.0, 1.0]
+]
+sensor.declination = -15.9
+
+
+while True:
+    print(sensor.get_bearing())
+    time.sleep(0.5)
